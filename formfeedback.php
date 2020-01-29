db8f8ccda6963a0dca8ed2921836333e75fb395c
Lakshmanan-coder <58321675+Lakshmanan-coder@users.noreply.github.com>
2020-01-29 04:01:34 -0800

    feedback mail integration

---
+++ formfeedback.php
@@ -0,0 +1,28 @@
+<?php
+$name=$_POST["Firstname"];
+$lname=$_POST["Lastname"];
+$email=$_POST["email"];
+$phone=$_POST["phone"];
+$safety=$_POST["safety"];
+$travel=$_POST["travel"];
+$refer=$_POST["refer"];
+
+
+// $phone=$_POST["phone"];
+$message=$_POST["message"];
+$to="lakshvj21@gmail.com"; // Receiver Email ID, Replace with your email ID
+			            	$subject='Contact Form Submit from OnewayHire cab';
+						
+                            $headers="From: ".$email;
+							 
+							 $msg="Name : $name $lname \n\n Email: $email \n\n \n\n Phone:$phone \n\n Safety,politness & performance $safety\n\n Would you travel again with oneway hire cab in future $travel \n\n Would you refer to others $refer \n\n Feedback: $message";
+							$retval = mail ($to,$subject,$msg,$headers);
+							if($retval == true){
+							echo 'success';
+								
+							}
+							else{
+							echo 'failed';
+								
+							}
+?>
