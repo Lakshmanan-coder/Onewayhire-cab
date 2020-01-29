7768d3666c047b47f7f979f39192f9d15641f463
Lakshmanan-coder <58321675+Lakshmanan-coder@users.noreply.github.com>
2020-01-29 04:18:45 -0800

    Update formfeedback.php

--- formfeedback.php
+++ formfeedback.php
@@ -11,14 +11,14 @@ $refer=$_POST["refer"];
 // $phone=$_POST["phone"];
 $message=$_POST["message"];
 $to="lakshvj21@gmail.com"; // Receiver Email ID, Replace with your email ID
-			            	$subject='Contact Form Submit from OnewayHire cab';
+			            	$subject='Feedback form Submit from OnewayHire cab';
 						
                             $headers="From: ".$email;
 							 
 							 $msg="Name : $name $lname \n\n Email: $email \n\n \n\n Phone:$phone \n\n Safety,politness & performance $safety\n\n Would you travel again with oneway hire cab in future $travel \n\n Would you refer to others $refer \n\n Feedback: $message";
 							$retval = mail ($to,$subject,$msg,$headers);
 							if($retval == true){
-							echo 'success';
+							echo 'success feedback';
 								
 							}
 							else{
