9cb01c8e5b5cfef157a9294669e94897f758b673
Lakshmanan-coder <58321675+Lakshmanan-coder@users.noreply.github.com>
2020-02-20 04:08:38 -0800

    Update map.js

--- map.js
+++ map.js
@@ -29,8 +29,14 @@ search2.addListener('places_changed',()=>{
 })
 
 async function distanceCalculator(){
-   const data = await fetch("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=12.951611%2080.14616629999999&destinations=13.1076828%2080.1522449&key=AIzaSyBxgaV6J5TXybOy9-ZuyfIv7V_JFM47u-0")
-     console.log(data)
+   const data = await fetch("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=12.951611%2080.14616629999999&destinations=13.1076828%2080.1522449&key=AIzaSyBxgaV6J5TXybOy9-ZuyfIv7V_JFM47u-0",{
+  mode: 'cors',
+  headers: {
+    'Access-Control-Allow-Origin':'*'
+  }
+})
+   const response = await data.json()
+   console.log(response)
 } 
 
 distanceCalculator();
