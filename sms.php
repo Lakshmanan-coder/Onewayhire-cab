cb233a8a42cd624c821ad6a2a71cf7f79f975620
Lakshmanan-coder <58321675+Lakshmanan-coder@users.noreply.github.com>
2020-02-20 09:06:17 -0800

    Update sms.php

--- sms.php
+++ sms.php
@@ -20,7 +20,7 @@ $url="https://www.sms4india.com/api/v1/sendCampaign";
 $message = urlencode($msg);// urlencode your message
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_POST, 1);// set post data to true
-curl_setopt($curl, CURLOPT_POSTFIELDS, "apikey=7H05EN9RGMFYV3QB995QQMP473OO9MMT&secret=Q9JHEPEP0124TH5P&usetype=stage&phone=$phone&senderid=oneway&message=$message");// post data
+curl_setopt($curl, CURLOPT_POSTFIELDS, "apikey=K8ONYLPMH3I4W2E2G465E8GO9CISWX7I&secret=M2BZSS32X1ERFAAK&usetype=stage&phone=$phone&senderid=oneway&message=$message");// post data
 // query parameter values must be given without squarebrackets.
  // Optional Authentication:
 curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
