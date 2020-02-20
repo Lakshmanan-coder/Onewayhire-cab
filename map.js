cd0a8415e2f2a2aa83bbe4441f1b2908c6b8670e
Lakshmanan-coder <58321675+Lakshmanan-coder@users.noreply.github.com>
2020-02-20 04:16:42 -0800

    Update map.js

--- map.js
+++ map.js
@@ -29,12 +29,9 @@ search2.addListener('places_changed',()=>{
 })
 
 async function distanceCalculator(){
-   const data = await fetch("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=12.951611%2080.14616629999999&destinations=13.1076828%2080.1522449&key=AIzaSyBxgaV6J5TXybOy9-ZuyfIv7V_JFM47u-0",{
-  mode: 'cors',
-  headers: {
-    'Access-Control-Allow-Origin':'*'
-  }
-})
+  const proxy = "https://cors-anywhere.herokuapp.com/";
+  const url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=12.951611%2080.14616629999999&destinations=13.1076828%2080.1522449&key=AIzaSyBxgaV6J5TXybOy9-ZuyfIv7V_JFM47u-0";
+   const data = await fetch(proxy + url);
    const response = await data.json()
    console.log(response)
 } 
