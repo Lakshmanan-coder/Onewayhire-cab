d14486e890fafbe1df2b6c0ca933b256282a706e
Lakshmanan-coder <58321675+Lakshmanan-coder@users.noreply.github.com>
2020-02-27 01:55:22 -0800

    theme changes and sms php changes

--- sms.php
+++ sms.php
@@ -19,8 +19,8 @@ $msg= "Success\nContact:8610080366 Pickup-Location: $pickupl Pickup-Time: $picku
 
 // echo $msg;
 
-$to="lakshvj21@gmail.com"; // Receiver Email ID, Replace with your email ID
-			            	$subject='Contact Form - OnewayHire cab';
+$to="onewayhirecab@gmail.com"; // Receiver Email ID, Replace with your email ID
+			            	$subject='Customer Request Form - OnewayHire cab';
 						
                             $headers="From: ".$email;
 							 
@@ -35,17 +35,17 @@ $to="lakshvj21@gmail.com"; // Receiver Email ID, Replace with your email ID
 								
 							}
 
-// $url="https://www.sms4india.com/api/v1/sendCampaign";
-// $message = urlencode($msg);// urlencode your message
-// $curl = curl_init();
-// curl_setopt($curl, CURLOPT_POST, 1);// set post data to true
-// curl_setopt($curl, CURLOPT_POSTFIELDS, "apikey=TDBFEOEB2HJMZXPLUX43BP6MHOACAMW1&secret=NB139GYO72HVQT0P&usetype=stage&phone=$phone&senderid=oneway&message=$message");// post data
-// // query parameter values must be given without squarebrackets.
-//  // Optional Authentication:
-// curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-// curl_setopt($curl, CURLOPT_URL, $url);
-// curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-// $result = curl_exec($curl);
-// curl_close($curl);
-// echo $result;
+$url="https://www.sms4india.com/api/v1/sendCampaign";
+$message = urlencode($msg);// urlencode your message
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_POST, 1);// set post data to true
+curl_setopt($curl, CURLOPT_POSTFIELDS, "apikey=TDBFEOEB2HJMZXPLUX43BP6MHOACAMW1&secret=NB139GYO72HVQT0P&usetype=stage&phone=$phone&senderid=oneway&message=$message");// post data
+// query parameter values must be given without squarebrackets.
+ // Optional Authentication:
+curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($curl);
+curl_close($curl);
+echo $result;
 ?>
